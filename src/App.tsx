17b071213f5f239039824759cfb45440a0981e17
PePacheco <pedro_pacheco98@outlook.com>
2021-08-29 17:08:44 -0300

    Adding mirajejs to make fake api calls and the modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+	function didOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true)
+	} 
+
+	function didCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false)
+	}
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={didOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal 
+					isOpen={isNewTransactionModalOpen} 
+					onRequestClose={didCloseNewTransactionModal}
+				>
+					<h2>Cadastrar Transacão</h2>
+				</Modal>
+
       <GlobalStyle />
     </>
   );
